7181b1df1f8ed1e7176f93d58e690a9c236b60a9
lordsilou <kevin_corne@hotmail.com>
2016-10-03 11:32:10 +0200

    Full Database Seeder which seeds everything at once (running php artisan db:seed). If you have any errors just run 'composer update' in var/www and re-migrate before seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(BlocktypesTableSeeder::class);
+        $this->call(MaterialtypesTableSeeder::class);
+        $this->call(PrimesilosTableSeeder::class);
+        $this->call(WastesilosTableSeeder::class);
     }
 }
