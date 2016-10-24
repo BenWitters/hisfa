cfc04b50038c6b0775f1b55da3e6a29b34e98845
iljaschoepen <ilja.schoepen@student.thomasmore.be>
2016-10-24 19:50:13 +0200

    User can delete a prime silo

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', 'HomeController@index');
 Route::get('/account', "UserController@index");
 Route::get('/waste', "WasteController@index");
 Route::get('/silos', "PrimesiloController@index");
-
+Route::delete('/silos/{id}', "PrimesiloController@delete");
 
 Route::get('/blocks', function () {
     return view('blocks/blocks');
