362ac33f20f1973223e98e0dd634411158ee8521
lordsilou <kevin_corne@hotmail.com>
2016-10-26 14:00:01 +0200

    fix fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,12 +14,11 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(BlocktypesTableSeeder::class);
         $this->call(MaterialtypesTableSeeder::class);
+
         //$this->call(MaterialsTableSeeder::class);
-<<<<<<< HEAD
         //$this->call(PrimesilosTableSeeder::class);
-=======
+        
         $this->call(PrimesilosTableSeeder::class);
->>>>>>> 7088aacf2c0dd06fb17a11fdf84aa7895d883d9b
         $this->call(WastesilosTableSeeder::class);
     }
 }
