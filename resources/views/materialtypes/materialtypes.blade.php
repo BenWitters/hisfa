6e32aff8eeebb4f6239ee4535db8d615208a7e01
iljaschoepen <ilja.schoepen@student.thomasmore.be>
2016-11-28 16:47:25 +0100

    Grondstoffen Index redesign

--- resources/views/materialtypes/materialtypes.blade.php
+++ resources/views/materialtypes/materialtypes.blade.php
@@ -1,79 +1,46 @@
-<head>
-    <title>Materialtypes</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
+@extends('layouts.app')
 
-    <nav class="navbar navbar-inverse">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="{{ URL::to('materialtypes') }}">Materialtypes</a>
+@section('content')
+
+    <header class="view__sub blocks">
+        <div class="wrapper">
+            <h1 class="view__sub__title">Grondstoffen</h1>
+
+            <a href="{{ URL::to('/materialtypes/create') }}" class="view__sub__button button">Grondstof toevoegen</a>
+
         </div>
-        <ul class="nav navbar-nav">
-            <li><a href="{{ URL::to('materialtypes') }}">View All The Materialtypes</a></li>
-            <li><a href="{{ URL::to('materialtypes/create') }}">Create a Materialtype</a>
-        </ul>
-    </nav>
 
-    <h1>All the Materialtypes</h1>
+        <div class="view__top__overlay"></div>
+    </header>
 
-    <!-- will be used to show any messages -->
-    @if (Session::has('message'))
-        <div class="alert alert-info">{{ Session::get('message') }}</div>
-    @endif
+    <div class="content wrapper">
+        <div class="wrapper--grondstoffen">
 
-    <table class="table table-striped table-bordered">
-        <thead>
-        <tr>
-            <td>ID</td>
-            <td>materialTypes</td>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($materialtypes as $key => $value)
-            <tr>
-                <td>{{ $value->id }}</td>
-                <td>{{ $value->material_type_name }}</td>
-
-
-                <td>
-                    <!-- show  -->
-                    <a class="btn btn-small btn-success" href="{{ URL::to('materialtypes/' . $value->id) }}">Show this Materialtype</a>
-
-                    <!-- edit -->
-                    <a class="btn btn-small btn-info" href="{{ URL::to('materialtypes/' . $value->id . '/edit') }}">Edit this Materialtype</a>
-                </td>
-                <td>
-                    <!-- Octabins -->
-                    Octabins: {{ $value->amount }}
-
+            @foreach($materialtypes as $key => $value)
+            <div class="grondstofitem">
+                <div class="grondstofitem__header">
+                    <div class="grondstofitem__header__overlay">
+                        <h2 class="grondstofitem__title">{{ $value->material_type_name }}</h2>
+                        <span class="grondstofitem__amount"><span class="grondstofitem__amount__count">{{ $value->amount }}</span> ton</span>
+                    </div>
+                </div>
+                <div class="grondstofitem__action">
                     <!-- Add octabin -->
                     {!! Form::open(array('url' => 'materialtypes/' . $value->id . '/octabin', 'class' => 'pull-right')) !!}
                     {!! Form::hidden('_method', 'POST') !!}
-                    {!! Form::submit('Add 1', array('class' => 'btn btn-info')) !!}
+                    {!! Form::submit('+1 octabin', array('class' => 'btnOctabin btnoctabin--add button button--dark')) !!}
                     {!! Form::close() !!}
-
                     <!-- Delete octabin -->
                     {!! Form::open(array('url' => 'materialtypes/' . $value->id . '/octabin', 'class' => 'pull-right')) !!}
                     {!! Form::hidden('_method', 'DELETE') !!}
-                    {!! Form::submit('Delete 1', array('class' => 'btn btn-danger')) !!}
+                    {!! Form::submit('-1 octabin', array('class' => 'btnOctabin btnOctabin--delete button button--dark')) !!}
                     {!! Form::close() !!}
-                </td>
-                <td>
-                    <!-- delete -->
-                    {{--<a class="btn btn-small btn-danger" href="{{ URL::to('blocktypes/' . $value->id . '/delete') }}">Delete this Blocktype</a>--}}
-                    {!! Form::open(array('url' => 'materialtypes/' . $value->id, 'class' => 'pull-right')) !!}
-                    {!! Form::hidden('_method', 'DELETE') !!}
-                    {!! Form::submit('Delete this Materialtype', array('class' => 'btn btn-warning')) !!}
-                    {!! Form::close() !!}
+                </div>
+            </div>
+            @endforeach
+        </div>
 
 
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+    </div>
 
-</div>
-</body>
-</html>
+@endsection
